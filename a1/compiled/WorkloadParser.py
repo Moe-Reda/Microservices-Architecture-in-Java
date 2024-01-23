bfc87085b2b55682e1f198e6ed02d4cb5d88cc3b
Moe-Reda <108328374+Moe-Reda@users.noreply.github.com>
2024-01-23 13:53:38 -0500

    directory structure and first attempt at WorkloadParser

---
+++ a1/compiled/WorkloadParser.py
@@ -0,0 +1,79 @@
+import requests
+import json
+import sys
+
+def format_data(data):
+    if data[0] == 'USER':
+        data = {
+            'command': data[1],
+            'id': data[2],
+            'username': data[3],
+            'email': data[4],
+            'password': data[5]
+        }
+    elif data[0] == 'PRODUCT':
+        data = {
+            'command': data[1],
+            'name': data[2],
+            'description': data[3],
+            'price': data[4],
+            'quantity': data[5]
+        }
+    elif data[0] == 'ORDER':
+        data = {
+            'command': data[1],
+            'product_id': data[2],
+            'user_id': data[3],
+            'quantity': data[4]
+        }
+
+def read_config(config_file):
+    try:
+        with open(config_file, 'r') as f:
+            config = json.load(f)
+            return config
+    except FileNotFoundError:
+        print(f"Error: Config file '{config_file}' not found.")
+        sys.exit(1)
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON in config file: {e}")
+        sys.exit(1)
+
+def send_request(api_url, request):
+    try:
+        if request[1] in ['info', 'get']:
+            response = requests.get(api_url + str(request[2]))
+        else:
+            data = format_data(request)
+            response = requests.post(api_url, data=data)
+        print(f"Request: {request.strip()} | Response: {response.text}")
+    except requests.RequestException as e:
+        print(f"Error sending request: {e}")
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python WorkloadParser.py <requests_file> <config_file>")
+        sys.exit(1)
+
+    requests_file = sys.argv[1]
+    config_file = sys.argv[2]
+
+    # Read config
+    config = read_config(config_file)
+
+    # Construct API URL
+    api_url = f"http://{config['OrderService']['ip_address']}:{config['OrderService']['port']}/"
+
+    # Read requests from file and send them to the API
+    try:
+        with open(requests_file, 'r') as file:
+            for line in file:
+                command = line.split()
+                endpoint = command[0].lower
+                send_request(api_url + endpoint, command)
+    except FileNotFoundError:
+        print(f"Error: Requests file '{requests_file}' not found.")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
