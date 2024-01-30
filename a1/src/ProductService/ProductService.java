49269c9cef7ac4f30a294003746521778694ddd6
Moe-Reda <108328374+Moe-Reda@users.noreply.github.com>
2024-01-29 19:38:58 -0500

    testing

--- a1/src/ProductService/ProductService.java
+++ a1/src/ProductService/ProductService.java
@@ -8,6 +8,7 @@ import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
