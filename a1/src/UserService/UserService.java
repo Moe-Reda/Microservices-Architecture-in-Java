a4f5e80fcf7a55c2eef8713ef1e172b569087d14
Moe-Reda <108328374+Moe-Reda@users.noreply.github.com>
2024-01-30 17:00:29 -0500

    it be the smallest things

--- a1/src/UserService/UserService.java
+++ a1/src/UserService/UserService.java
@@ -91,7 +91,7 @@ public class UserService {
                     String params = clientUrl.substring(index);
 
                     //Checking if the request is valid
-                    if(!ServiceUtil.isNumeric(params)){
+                    if(!ServiceUtil.isNumeric(params.substring(1))){
                         responseMap.put("rcode", "400");
                     } else{
                         //Execute query
